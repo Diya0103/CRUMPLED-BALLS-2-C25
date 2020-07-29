fac893ba827cd1d4d7e7ad13407f2214bf202df9
Diya0103 <66773803+Diya0103@users.noreply.github.com>
2020-07-29 20:23:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+var paper,bin;
+var binImg,paperImg,box4;
+
+function preload() {
+	
+}
+	
+
+//1500,600
+function setup() {
+	createCanvas(900, 500);
+
+	
+    
+	engine = Engine.create();
+	Engine.run(engine);
+
+	world = engine.world;
+
+	//packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:false});
+	//World.add(world, packageBody);
+	
+
+	//Create a Ground
+	//ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	//World.add(world, ground);
+	paper = new Paper();
+	ground = new Ground();
+	bin = new Dustbin();
+	
+
+
+	
+  
+}
+function keyPressed(){
+
+	if(keyCode == UP_ARROW){
+
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:65,y:-55})
+
+	}
+
+}
+
+
+
+function draw() {
+  rectMode(CENTER);
+  background("white");
+  ground.display();
+  paper.display();
+  bin.display();
+ 
+}
+
+
+
